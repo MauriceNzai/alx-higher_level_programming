ecef94438c756c40fa5dc442b4ae2395f963b005
Maurice <maurice.nzai@gmail.com>
2022-09-24 13:47:09 +0000

    Update Task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@ def multiple_returns(sentence):
     num = len(sentence)
     if (len(sentence) > 0):
         c = sentence[0]
-    else:
+    elif (len(sentence) == 0):
         c = None
     my_tuple = (num, c)
     return(my_tuple)
