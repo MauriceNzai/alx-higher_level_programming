7d6c3fa0b46e8be1d9ac72da209b2bf44f4d2b89
Maurice <maurice.nzai@gmail.com>
2022-09-24 13:56:55 +0000

    Update Task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+    if (len(my_list) == 0):
+        return (None)
+
     #step1. Initalize idx 0 as the largest
     max_int = my_list[0]
     #step2. iterate list and check each if > than max_int
-    for num in my_list[1:]:
-        if num > max_int:
+    for num in range(1, len(my_list)):
+        if my_list[num] > max_int:
             # assign num to max_int
-            max_int = num
+            max_int = my_list[num]
     #return max_int
     return (max_int)
