de5aa2717708717f7a8fd24d9c41c5906ca5c5ac
Maurice <maurice.nzai@gmail.com>
2022-10-11 11:00:18 +0000

    Task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+
+    count = 0
+
+    while (my_list):
+        for i in range(x):
+            try:
+                print("{}".format(my_list[i]), end="")
+            except IndexError:
+                break
+            else:
+                count += 1
+
+    print()
+    return (count)
