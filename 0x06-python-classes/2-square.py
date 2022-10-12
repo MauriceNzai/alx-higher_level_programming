3ee768482a09a6b067ae0e26c3ddfcf09b8860b6
Maurice <maurice.nzai@gmail.com>
2022-10-12 13:46:29 +0000

    Task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+class Square:
+
+    """
+    Modelling a square class with private attribute size
+    and instatiation with optional size
+    """
+    def __init__(self, size=0):
+        if(type(size) is not int):
+            raise TypeError("size must be an integer")
+        if(size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+        """
+        initializes instance of a square
+        Args:
+            __size(int): size of square
+            size must be positive and integer type
+        """
