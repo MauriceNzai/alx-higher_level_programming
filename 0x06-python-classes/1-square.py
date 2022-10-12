d9a8c4a82047582eedd1d3e3b72eb09ec816f8fb
Maurice <maurice.nzai@gmail.com>
2022-10-12 13:43:07 +0000

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+class Square:
+
+    """
+    Modelling a square class
+    and defines it by attribut size
+    and instatiates with size
+    """
+
+    def __init__(self, size):
+        self.__size = size
+        
+    """
+    initializes a square with parameters
+    args:
+        __size(int): size of the square
+    """
