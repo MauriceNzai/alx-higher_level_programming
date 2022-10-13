7b6c428e2a45353d43a674223480337a309fd7df
Maurice <maurice.nzai@gmail.com>
2022-10-13 07:46:22 +0000

    add doc string

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 class Square:
-    """
-    Modelling an empty square class that defines a square
+    """Modelling an empty square class that defines a square
     """
     pass
