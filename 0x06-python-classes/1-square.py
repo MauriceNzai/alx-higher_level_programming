8cc4ebc7699bbf8d1397ba37b188f12b1f125a8d
Maurice <maurice.nzai@gmail.com>
2022-10-13 08:04:09 +0000

    Update Task

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 class Square:
     """
-    Modelling a square class
-    and defines it by attribut size
-    and instatiates with size
+    Modelling a square class with attributes and instatiates with size
+
+    Attributes:
+        size (int): The size of the square
     """
 
     def __init__(self, size):
+        """
+        initializes a square with parameters
+
+        args:
+            __size(int): size of the square
+        """
+
         self.__size = size
-    """
-    initializes a square with parameters
-    args:
-        __size(int): size of the square
-    """
