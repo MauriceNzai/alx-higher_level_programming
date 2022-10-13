a86b5c87768312ea4ac9f5e01a1147b58977c7be
Maurice <maurice.nzai@gmail.com>
2022-10-13 08:33:30 +0000

    Update file

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 12 2022
+@author: Maurice Haro
+"""
+
 
 class Square:
     """Modelling an empty square class that defines a square
