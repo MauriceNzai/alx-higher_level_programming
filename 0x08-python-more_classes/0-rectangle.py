54f5b77ad7ed44f92e74476e8373ef60a8b40bd1
Maurice <maurice.nzai@gmail.com>
2022-10-17 09:46:33 +0000

    Add Task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+This module contains an empty rectangle class
+Created on Mon Oct 17 2022
+@author: Maurice Haro
+"""
+
+
+class Rectangle:
+    """
+    Empty class Rectangle defining a simple rectangle
+
+    Attributes:
+        empty
+    """
+
+    pass
