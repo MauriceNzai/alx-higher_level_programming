52dffb9ba00798f454f243b511aecdb462d8caa8
Maurice <maurice.nzai@gmail.com>
2022-10-21 09:45:03 +0000

    Add Task 2

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module consists of a function that prints the first and last names
+
+Created on Tue Oct 18 2022
+@author: Maurice Haro
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints the "My name is + first and last name
+    
+    Args:
+        first_name (str): First string to print
+        last_name (str): Second string to print
+
+    Raises:
+        TypeError: Exception is arguments not strings
+    """
+    if type(first_name) is not str:
+        raise TypeError("first name must be a string")
+            
+    elif type(last_name) is not str:
+        raise TypeError("last name must be a string")
+    else:
+        print("My name is {} {}".format(first_name, last_name))
---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def magic_string(B=[]):
+    B += ["BestSchool"]
+    return (", ".join(B))
+
