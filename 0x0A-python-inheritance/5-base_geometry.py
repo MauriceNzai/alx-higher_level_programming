3365867183c7400e3a8adf83f0535f2c2ded5b47
Maurice <maurice.nzai@gmail.com>
2022-10-24 12:06:03 +0000

    Add Task 5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+basegeomentryclass
+"""
+
+
+class BaseGeometry:
+    """
+    instance of geometry class
+    """
+    def __init__(self):
+        """
+        initializing instance
+        """
+        pass
