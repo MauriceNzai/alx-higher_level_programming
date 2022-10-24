5805381f5d28338f53a05c0f4f9d28317590f079
Maurice <maurice.nzai@gmail.com>
2022-10-24 12:06:24 +0000

    Add Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+basegeomentryclass
+"""
+
+
+class BaseGeometry:
+    """
+    instance of geometry class
+    """
+    def __init__(self):
+        """
+        initializing instance
+        """
+    def area(self):
+        """
+        raise exception
+        """
+        raise Exception("area() is not implemented")
