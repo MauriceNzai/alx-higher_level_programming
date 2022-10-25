05bb1162860737754dc71d40462edb1964cf2058
Maurice <maurice.nzai@gmail.com>
2022-10-25 09:54:36 +0000

    Add Task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+function read_file to read a file and display on stdo
+Created on Tue Oct, 25 2022
+
+@author: Maurice Haro
+"""
+def read_file(filename=""):
+    """
+    reads a file and displays to stdo
+    """
+    with open(filename, mode='r', encoding='utf-8') as f:
+        for line in f:
+            print(line, end="")
