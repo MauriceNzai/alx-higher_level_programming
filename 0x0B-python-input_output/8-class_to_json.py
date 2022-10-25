589622a78834522474b6c1944b303adb54ae9d11
Maurice <maurice.nzai@gmail.com>
2022-10-25 12:56:59 +0000

    Add Task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module that creates a function returning a dictionary description
+
+Created on Tue Oct, 25 2022
+
+@author: Maurice Haro
+"""
+
+
+def clas_to_json(obj):
+    """
+    Creates a JSON representation of an object
+
+    Argumente:
+        obj (obj): The object to create a class of
+
+
+    Return:
+        A JSON representation
+    """
+
+
+    return {key: value for (key, value) in obj.__dict__.items()
+            if key in list(obj.__dict__.keys())}
