15efe498b7394bb3f5cf9b1e2927e1ef258728f0
Maurice <maurice.nzai@gmail.com>
2022-10-26 11:27:08 +0000

    Check Style

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -26,11 +26,12 @@ class Student:
             last_name (str): student's last name
             age (int): student's age
         """
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs = None):
+    def to_json(self, attrs=None):
         """
         retrieves a dictionary representation of  a Student
 
