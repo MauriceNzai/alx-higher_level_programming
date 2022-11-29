fdf825a8c592dee09366afa205d2d339e0adc975
Maurice <maurice.nzai@gmail.com>
2022-11-29 12:19:07 +0000

    Add Task 10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Lists all records of the second_table in the database hbtn_0c_0 in MySQL server
+-- Displaying the name and score, ordered by score, descending oreder
+-- The database name will be passed as argument of mysql command
+SELECT score, name FROM second_table ORDER BY score DESC;
