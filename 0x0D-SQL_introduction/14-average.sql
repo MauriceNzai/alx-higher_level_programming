36f78ce352650d8c574a0d7f742842539edb16d7
Maurice <maurice.nzai@gmail.com>
2022-11-29 12:20:00 +0000

    Add Task 14

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- Computes score average of all records in second_table of database hbtn_0c_0
+-- The result column name is average
+-- The database name will be passed as argument of mysql command
+SELECT AVG(score) AS 'average' FROM second_table;
