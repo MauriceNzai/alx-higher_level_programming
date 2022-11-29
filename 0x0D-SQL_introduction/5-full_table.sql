369585f49cfb9f3cc1de5b68d4e03b70e38b1762
Maurice <maurice.nzai@gmail.com>
2022-11-29 12:17:49 +0000

    Add Task 5

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- This script prints the full description of the table first_table
+-- from the databse hbtn_0c_0 in MySQL server
+-- The database name will be passed as argument of mysql command
+SHOW CREATE TABLE first_table;
