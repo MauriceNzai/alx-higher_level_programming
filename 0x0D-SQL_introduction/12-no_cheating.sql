08199ce80aef116230971c0bf2c2c156f2ec8cb1
Maurice <maurice.nzai@gmail.com>
2022-11-29 12:19:32 +0000

    Add Task 12

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,4 @@
+-- Updates score of Bob to 10 in second_table in database hbtn_0c_0
+-- Only allowed to use the name field
+-- The database name will be passed as argument of mysql command
+UPDATE second_table, SET score = 10 WHERE name = 'Bob';
