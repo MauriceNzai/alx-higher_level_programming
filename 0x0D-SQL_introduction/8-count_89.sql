bc723e9f26254acd894e3e750e78786152b586dd
Maurice <maurice.nzai@gmail.com>
2022-11-29 12:30:07 +0000

    Update file

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,4 @@
 -- This script lists the number of records with a given id first_table
 -- from the database hbtn_0c_0 in MySQL server
 -- The database name will be passed as argument of mysql command
-SELECT COUNT(*) FROM first_tabl WHERE id=89;
+SELECT COUNT(*) FROM first_table WHERE id=89;
