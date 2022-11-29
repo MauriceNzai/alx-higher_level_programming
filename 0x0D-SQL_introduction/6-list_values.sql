c808423545fccaf8a836dcb2873a867599ea1aaf
Maurice <maurice.nzai@gmail.com>
2022-11-29 12:18:24 +0000

    Add Task 6

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,4 @@
+-- This script lists all rows of the table first_table
+-- from the databse hbtn_0c_0 in MySQL server
+-- The database name will be passed as argument of mysql command
+SELECT * FROM first_table;
