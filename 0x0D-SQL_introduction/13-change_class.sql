7bde4f01733e8630eb2ee401db24373e11782e3b
Maurice <maurice.nzai@gmail.com>
2022-11-29 12:19:48 +0000

    Add Task 13

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- Deletes records with score <= 5 in the second_table of database hbtn_0c_0
+-- The database name will be passed as argument of mysql command
+DELETE FROM second_table, WHERE score <= 5;
