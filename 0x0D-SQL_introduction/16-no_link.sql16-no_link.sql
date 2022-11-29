e952ede0bd36b3ae328ca0a51e5aa6d7adc8a05d
Maurice <maurice.nzai@gmail.com>
2022-11-29 12:43:50 +0000

    no file

--- 0x0D-SQL_introduction/16-no_link.sql16-no_link.sql
+++
@@ -1,5 +0,0 @@
--- Lists records of second_table of hbtn_0c_0
--- Leaving out rows without a name value
--- Result should display score, name in descending order
--- The database name will be passed as argument of mysql command
-SELECT score, name FROM second_table WHERE name != '' ORDER BY score DESC;
