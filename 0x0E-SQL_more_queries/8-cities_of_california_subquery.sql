9d713ccf77f2bc0d59a4f89d0afcf224b3cc5df1
Maurice <maurice.nzai@gmail.com>
2022-11-30 11:46:50 +0000

    Add Task 8

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,10 @@
+-- Lists all cities of California found in the database
+-- Results sorted in ascending order by cities.id
+-- Name of the database will be passed as an argument of the mysql command
+
+-- use the new database
+use hbtn_0d_usa;
+-- create the main query followed by the subquery
+SELECT * FROM cities WHERE (
+	state_id = SELECT id FROM states WHERE name='California')
+	ORDER BY cities.id ASC;
