f78880c836216c05289abff6958317fac015d206
Maurice <maurice.nzai@gmail.com>
2022-11-30 11:44:56 +0000

    Add Task 1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Creates user with all previledges on MySQL server
+-- Sets user password and script should not fail if user exists
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- Add priviledges to the user
+GRANT ALL PRIVILEDGES ON *.* TO 'user_0d_1'@'localhost';
