ec93db3c00156a3a33ed8c3300a6d164cb0a480f
Maurice <maurice.nzai@gmail.com>
2022-11-30 11:45:39 +0000

    Add Task 3

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,4 @@
+-- Creates a table force_name on MySQL server
+-- Name of the database will be passed as an argument of the mysql command
+-- Script should not fail if table already exists
+CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL);
