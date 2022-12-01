ddd905ab0141a496d791fd618f115f0be43a9929
Maurice <maurice.nzai@gmail.com>
2022-12-01 12:25:43 +0000

    Update File

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,6 @@
 -- Creates user with all previledges on MySQL server
 -- Sets user password and script should not fail if user exists
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 -- Add priviledges to the user
 GRANT ALL PRIVILEDGES ON *.* TO 'user_0d_1'@'localhost';
+ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
