af0cdbcffd033676cde5c997bd78903e29cfc957
Maurice <maurice.nzai@gmail.com>
2022-12-02 09:01:07 +0000

    Update File

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,8 @@
--- genre id by show
-SELECT tgg.name FROM tv_genres AS tgg
-JOIN tv_show_genres AS tgs
-ON tgs.genre_id = tgg.id
-JOIN tv_shows AS ts
-ON ts.id = tgs.show_id
-WHERE ts.title='Dexter'
-GROUP BY tgg.name ASC;
+-- Lists all genres of the show Dexter
+SELECT name FROM tv_genres
+JOIN tv_show_genres
+ON tv_show_genres.genre_id = tv_genres.id
+JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show.title='Dexter'
+GROUP BY tv_genres.name ASC;
