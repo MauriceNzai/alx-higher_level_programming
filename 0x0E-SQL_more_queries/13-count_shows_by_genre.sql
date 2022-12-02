3c489e6f9312658068d5949f58c48cb0b3e12043
Maurice <maurice.nzai@gmail.com>
2022-12-02 08:46:33 +0000

    Update File

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,8 @@
--- genre id by show
-SELECT tv_genre.name AS genre,
+-- List tv shows and their genres
+-- List all genres from the database and
+-- display the number of shows linked to each genre
+
+SELECT tv_genres.name AS genre,
 COUNT(tv_show_genres.genre_id) AS number_of_shows
 FROM tv_show_genres
 INNER JOIN tv_genres
