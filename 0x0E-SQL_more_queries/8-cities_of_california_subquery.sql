f4c460783ad36e7a4f97f565c310f6a0faf6bc62
Maurice <maurice.nzai@gmail.com>
2022-12-02 08:18:10 +0000

    Updte File

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -4,5 +4,5 @@
 
 -- create the main query followed by the subquery
 SELECT cities.id, cities.name FROM cities, states
-WHERE cities.state_id = states.id
+WHERE states.name = California
 ORDER BY cities.id ASC;
