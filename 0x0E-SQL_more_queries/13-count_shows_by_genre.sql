fd212205a370470bdf583bc372885fcad81c99a5
Maurice <maurice.nzai@gmail.com>
2022-12-02 08:42:35 +0000

    Update File

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,9 +1,8 @@
 -- genre id by show
-SELECT tgs.name AS genre, COUNT(tg.genre_id) AS number_shows
-FROM tv_shows AS ts
-LEFT JOIN tv_show_genres AS tg
-ON ts.id = tg.show_id
-LEFT JOIN tv_genres AS tgs
-ON tg.genre_id = tgs.id
-WHERE tg.genre_id IS NOT NULL
-GROUP BY tgs.name ORDER BY number_shows DESC;
+SELECT tv_genre.name AS genre,
+COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM tv_show_genres
+INNER JOIN tv_genres
+ON tv_show_genres.genre_id = tv.genres_id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
