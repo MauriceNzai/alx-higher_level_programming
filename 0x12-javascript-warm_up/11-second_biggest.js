5e0d342e9c281248bf3b9ac89ec2bfd7b0d510e6
Maurice <maurice.nzai@gmail.com>
2022-12-21 11:50:25 +0000

    Update file

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,5 +3,5 @@ let numbers = process.argv.slice(2).map(x => Number(x));
 if (numbers.length <= 1) {
 	console.log(0);
 } else {
-	console.log(numbers.sort().reverse()[1];
+	console.log(numbers.sort().reverse()[1]);
 }
