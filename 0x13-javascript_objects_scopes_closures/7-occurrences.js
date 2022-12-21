ca4994bab4d55b21a610e2f24724f9ae33a3d923
Maurice <maurice.nzai@gmail.com>
2022-12-21 13:21:05 +0000

    Update

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,7 +2,7 @@
 exports.nbOccurrences = function (list, searchElement) {
 	let count = 0;
 
-	for (let i =0; i < list.length; i++) {
+	for (let i = 0; i < list.length; i++) {
 		if (list[i] === searchElement) {
 			count += 1;
 		}
