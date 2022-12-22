3cadf93800e1dbb47ad519600cece964888bf383
Maurice <maurice.nzai@gmail.com>
2022-12-22 10:25:03 +0000

    Check semistandard notation

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 exports.nbOccurrences = function (list, searchElement) {
-	let count = 0;
+  let count = 0;
 
-	for (let i = 0; i < list.length; i++) {
-		if (list[i] === searchElement) {
-			count += 1;
-		}
-	}
-	return count;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      count += 1;
+    }
+  }
+  return count;
 };
