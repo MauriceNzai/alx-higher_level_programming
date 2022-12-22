f25476dfa3e98a1eb04353d6a875ad629a35e449
Maurice <maurice.nzai@gmail.com>
2022-12-22 10:55:49 +0000

    Check semistandard notation

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-let num = isNaN(Number(process.argv[2])) ? 1 : Number(process.argv[2]);
+const num = isNaN(Number(process.argv[2])) ? 1 : Number(process.argv[2]);
 function fact (a) {
-	if (a === 0) return 1;
-	return (a * fact(a - 1));
+  if (a === 0) return 1;
+  return (a * fact(a - 1));
 }
 
 console.log(fact(num));
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let phrases = ["C is fun", "Python is cool", "Javascript is amazing"];
-for (let i = 0; i < phrases.length; i++){
-	console.log(phrases[i]);
+const phrases = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < phrases.length; i++) {
+  console.log(phrases[i]);
 }
