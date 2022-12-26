fc74a1b791c48d293fb097214756bd37393c3c8c
Maurice <maurice.nzai@gmail.com>
2022-12-26 12:45:58 +0000

    Update

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,7 +6,7 @@ SQL Alchemy
 
 from sqlalchemy import create_engine
 from sqlalchemy import MetaData
-from sqlalchemy import sessionmaker
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 import sys
 
