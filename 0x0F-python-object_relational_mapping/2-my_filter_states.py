9f30c3d61f8b39d6311796b089ca29295d61da62
Maurice <maurice.nzai@gmail.com>
2022-12-26 11:30:38 +0000

    Add Task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# gets all states that  rgument passse by user
+
+
+def main(args):
+    """
+    Gets all states that match user's argument
+    """
+    if len(args) != 5:
+        raise Exception("need 4 arguments!")
+    db = MySQLdb.connect(host = "localhost", user = args[1],
+                        passwd = args[2], db = args[3])
+    cur = db.cursor()
+    cur.execute(
+            "SELECT * FROM states WHERE name like '{}' ORDER BY id ASC"
+            .format(args[4]))
+    states = cur.fetchall()
+    for state in states:
+        if state[1] == state[4]:
+            print(state)
+
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+    main(sys.argv)
