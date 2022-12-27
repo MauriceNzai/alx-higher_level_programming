05d02ae72259cbc720cb5e262d729781507fe533
Maurice <maurice.nzai@gmail.com>
2022-12-27 14:24:59 +0000

    Add Task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-# gets all cities
+"""
+contains a function main() that gets all cities
+"""
 
 
 def main(args):
@@ -8,13 +10,11 @@ def main(args):
     """
     if len(args) != 5:
         raise Exception("need 4 arguments!")
-    db = MySQLdb.connect(host = "localhost", user = args[1],
-                        passwd = args[2], db = args[3])
+    db = MySQLdb.connect(
+            host="localhost", user=args[1], passwd=args[2], db=args[3])
     cur = db.cursor()
-    cur.execute(
-            "SELECT c.name FROM cities c JOIN states s
-                    ON s.id = c.state_id WHERE s.name = %s ORDER BY c.id",
-                    (args[4],))
+    cur.execute("SELECT c.name FROM cities c JOIN states s\
+            ON s.id=c.state_id WHERE s.name=%s ORDER BY c.id", (args[4],))
     states = cur.fetchall()
     print(", ".join(map(lambda x: "%s" % x, states)))
 
