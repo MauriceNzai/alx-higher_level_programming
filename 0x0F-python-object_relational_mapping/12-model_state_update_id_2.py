7accad35bb3fdc1a18914134f8ed4111f9f83559
Maurice <maurice.nzai@gmail.com>
2022-12-27 13:10:34 +0000

    Add Task 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+from sqlalchemy import create_engine
+from sqlalchemy import MetaData
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+    engine = create_engine(
+            "mysql+mysqldb://{}:{}@localhost/{}".format(
+                sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    mexico = sessions.query(State).filter(
+            State.id == 23).first()
+    mexico.name = "New Mexico"
+    session.commit()
+    session.close()
