b794faa18e0ca54df69cb06641d790411ef55220
MauriceNzai <107055021+MauriceNzai@users.noreply.github.com>
2022-12-27 19:36:53 +0300

    Update 11-model_state_insert.py

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Contains a function that gets
+"""
+
+
 from sqlalchemy import create_engine
 from sqlalchemy import MetaData
 from sqlalchemy.orm import sessionmaker
@@ -8,7 +13,7 @@ import sys
 if __name__ == "__main__":
     engine = create_engine(
             "mysql+mysqldb://{}:{}@localhost/{}".format(
-                sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
