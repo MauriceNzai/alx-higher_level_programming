51b76b5aca2a19238e51320e97ecc782a082c9e1
Maurice <maurice.nzai@gmail.com>
2022-12-28 08:33:46 +0000

    Update Task 12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -11,14 +11,13 @@ from model_state import Base, State
 import sys
 
 if __name__ == "__main__":
-    engine = create_engine(
-            "mysql+mysqldb://{}:{}@localhost/{}".format(
-                sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    mexico = sessions.query(State).filter(
-            State.id == 23).first()
+    mexico = session.query(State).filter(
+            State.id == 2).first()
     mexico.name = "New Mexico"
     session.commit()
     session.close()
