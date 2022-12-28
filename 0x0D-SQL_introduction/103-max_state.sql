547d26b5cf0fb0873756776d3b924ad7706608b7
Maurice <maurice.nzai@gmail.com>
2022-12-28 13:33:35 +0000

    Update File

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,4 +1,6 @@
 -- Imports a table into hbtn_0c_0 database
 -- Displays max temperature of each state ordered by state name
-SELECT state, MAX(value) AS max_temp FROM temperatures
-GROUP BY state;
+SELECT state, MAX(value) AS max_temp
+FROM temperatures
+GROUP BY state
+LIMIT 3;
